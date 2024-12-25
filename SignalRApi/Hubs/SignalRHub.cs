b2d9a5406be020322ffc05b307430349c85edb2f
ondercebeci <69725787+ondercebeci@users.noreply.github.com>
2024-12-25 16:25:48 +0300

    SignalR Giriş

---
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Conctrete;
+
+namespace SignalRApi.Hubs
+{
+    public class SignalRHub:Hub
+    {
+        SignalRContext contex = new SignalRContext();
+
+        public async Task SendCategoryCount()
+        {
+            var value = contex.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+        }
+  
+
+    }
+}
